013ccbbbcb42d9e3cb076eafbf964b9ca49ac044
GeluFr <frateangelu@gmail.com>
2023-07-03 20:36:37 +0300

    Selenium

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+
+    @Test
+    public void googleTest() throws InterruptedException {
+        //instantiem driverul
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //deschidem google
+        String url = "https://www.google.ro/";
+        driver.get(url);
+
+        //maximizare pagina
+        driver.manage().window().maximize();
+
+        //verificam pagina curenta
+        Assert.assertEquals(url, driver.getCurrentUrl());
+
+        //inchidem pagina
+        driver.close();
+    }
+}
